c0748dc24c6734fc1b34bf156e93f5056f26ae22
sreejithkm2008 <sreejithkm2008@outlook.com>
2022-05-15 11:54:53 +0530

    initial commit

---
+++ NumberSystemConverter/Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Number
+{
+    public class Number
+    {
+        
+        private string _numbers;
+
+        public Number()
+        {
+        }
+
+        public int AddNumbers(string number_)
+        {
+            //if (number < 1 || number > 3000)
+            //{
+            //    throw  new IndexOutOfRangeException("The number supplied is out of the expected range (1 - 3000).");
+            //}
+            number_.Replace("/n", ",");
+            var numbersList = number_.Split(',');
+            int total = 0;
+            foreach (var number in numbersList)
+            {
+               int n= Convert.ToInt32(number);
+               if(n<0)
+                {
+                    throw new Exception("negatives not allowed");
+                }
+               else if(n>0)
+                {
+                    total += n;
+                }
+            }
+            return total;
+        }
+    }
+}
---
+++ NumberSystemConverter_UnitTests/NumberAdditionUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Number;
+
+namespace Number_UnitTests
+{
+    [TestClass]
+    public class NumberAdditionUnitTests
+    {
+
+        [TestMethod]
+        [ExpectedException(typeof (Exception))]
+        public void Number_Negative_ThrowsException_TestMethod()
+        {
+            var number = new Number.Number();
+            number.AddNumbers("-1,3");
+        }
+
+    }
+
+    [TestClass]
+    public class NumberAdditionExpectedValuesUnitTests
+    {
+
+        [TestMethod]
+        public void Number_Three_Expected_Result_I_TestMethod()
+        {
+            var number = new Number.Number();
+            var result = number.AddNumbers("1,2");
+
+            Assert.AreEqual(result, 3);
+
+        }
+
+    }
+}
